6a7c69bb93a1713c754b61b6172fa9ef589ddf30
ruchi kumari <02ruchi2002@gmail.com>
2025-07-24 22:01:39 -0700

    completed home page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+
+function App() {
+ return(
+  <div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/blog/:id' element={<Blog/>}/>
+    </Routes>
+  </div>
+ )
+}
+ 
+export default App
